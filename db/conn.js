0d623231a8dcb4ef2da0888163245cdafffe33ab
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 01:01:26 +0700

    first commit

--- db/conn.js
+++ db/conn.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
-  dialect: 'postgres',
+  dialect: 'mysql',
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
 });
